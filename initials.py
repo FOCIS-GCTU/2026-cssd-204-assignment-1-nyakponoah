0d35f5c8253da140970c1d2975ecf27408251578
Noah <2425402078@live.gctu.edu.gh>
2026-05-31 15:28:23 +0000

    Change grader name and clean up comments
    Updated grader name in comments and removed unnecessary comments.

--- initials.py
+++ initials.py
@@ -1,31 +1,20 @@
 # File: initials.py
 # Description: Print out stylized large initials for Nyakpo Noah.
 # Assignment Number: 1
-# 
 # Name: Nyakpo Noah
 # STUDENT ID: 2425402078
 # Email: 2425402078@live.gctu.edu.gh
-# Grader: Noah
-# 
+# Grader: Audustus Buckman
 # On my honor, Nyakpo Noah, this programming assignment is my own work
 # and I have not provided this code to any other student.
 
 
 def main():
-    # Print the student's stylized large initials using only print() and strings.
-    
-    # Blank line before small initials [cite: 20]
     print()
     
-    # Small initials [cite: 8, 24]
     print("...MNN")
     
-    # Blank line between small and large initials [cite: 20]
     print()
-    
-    # Large initials, 10 lines high and 12 characters wide each 
-    # Each followed by a large period made of 4 asterisks [cite: 14]
-    # Three periods columns to the left and right of each letter/period [cite: 15]
 
     print("...MM.MM..MM.MM........NNN......NNN........NNN......NNN.....")
     print("...MM.MM..MM.MM........NNNN.....NNN........NNNN.....NNN.....")
@@ -38,7 +27,6 @@ def main():
     print("...MM...MM...MM...**...NN......NNNN...**...NN......NNNN...**")
     print("...MM...MM...MM...**...NN.......NNN...**...NN.......NNN...**")
     
-    # Blank line after large initials [cite: 20]
     print()
 
 
